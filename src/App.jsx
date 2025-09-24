893d700e49ed42f6843b47c4c1066b1d97fd6a93
SM-Abbas-Dev-Dreamer <syedmuhammadabbasniqvi@gmail.com>
2025-09-24 01:01:13 -0700

    your update message

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/about";
 import Projects from "./pages/Projects";
